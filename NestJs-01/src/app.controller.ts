ebb769b0eed6516d3fb9a2964efbeedd71bb0572
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-04 21:30:18 +0530

    few change

--- NestJs-01/src/app.controller.ts
+++ NestJs-01/src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Param, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
