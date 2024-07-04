11fefd64eb30aa31b9e0362180eef5f782220073
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-04 21:32:42 +0530

    change folder structure for a more cli approach in NestJs

--- NestJs-01/src/app.module.ts
+++ NestJs-01/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
   controllers: [AppController]
 })
 export class AppModule { }
