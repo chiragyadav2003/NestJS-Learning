feba0d815b3a2e43ff6d4bb20c8ee07a4da09d27
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-09 11:53:29 +0530

    chore: Add Swagger documentation for user DTOs and API endpoints

--- NestJs-01/src/user/dto/create-user.dto.ts
+++ NestJs-01/src/user/dto/create-user.dto.ts
@@ -1,8 +1,17 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'user email',
+    default: "johndoe@gmail.com",
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'user password',
+    default: "password123",
+  })
   @IsNotEmpty()
   password: string;
 }
--- NestJs-01/src/user/dto/update-user.dto.ts
+++ NestJs-01/src/user/dto/update-user.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class UpdateUserDto {
+  @ApiProperty({
+    description: 'user email',
+    default: "johndoe@gmail.com",
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'user password',
+    default: "password123",
+  })
   @IsNotEmpty()
   password: string;
 }
