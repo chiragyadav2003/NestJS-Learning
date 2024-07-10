94348f805b022fc34fd545013ffff0b118d2f8d7
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-10 23:29:12 +0530

    dto update

--- NestJs-01/src/user/dto/create-user.dto.ts
+++ NestJs-01/src/user/dto/create-user.dto.ts
@@ -2,16 +2,16 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @ApiProperty({
+    description: 'user name',
+    default: "Chirag Yadav",
+  })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
     description: 'user email',
     default: "johndoe@gmail.com",
   })
   @IsEmail()
   email: string;
-
-  @ApiProperty({
-    description: 'user password',
-    default: "password123",
-  })
-  @IsNotEmpty()
-  password: string;
 }
--- NestJs-01/src/user/dto/update-user.dto.ts
+++ NestJs-01/src/user/dto/update-user.dto.ts
@@ -3,16 +3,16 @@ import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class UpdateUserDto {
   @ApiProperty({
+    description: 'user name',
+    default: "Chirag Yadav",
+  })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
     description: 'user email',
     default: "johndoe@gmail.com",
   })
   @IsEmail()
   email: string;
-
-  @ApiProperty({
-    description: 'user password',
-    default: "password123",
-  })
-  @IsNotEmpty()
-  password: string;
 }
