7b7684b8c9e6e7077083ab6642f895b0f97b7e7a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-10 23:29:37 +0530

    initialize typeorm

--- NestJs-01/src/app.module.ts
+++ NestJs-01/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   controllers: [AppController],
-  imports: [UserModule]
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ]
 })
 export class AppModule { }
