040640678627ea62343f5222316d48d5c3dc5bcb
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-12 16:17:43 +0530

    refactor: Update AuthModule to use LocalStrategy for authentication

--- NestJs-01/src/auth/auth.module.ts
+++ NestJs-01/src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
-  imports: [UserModule] // Import UserModule to use UserService in AuthService
+  imports: [UserModule, PassportModule] // Import UserModule to use UserService in AuthService
 })
 export class AuthModule { }
