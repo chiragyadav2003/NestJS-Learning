620ef8062517203bab7e8905574b979d275d1da4
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-12 16:58:34 +0530

    refactor: Update AuthService to use JwtService for token generation and validation

--- NestJs-01/src/auth/auth.service.ts
+++ NestJs-01/src/auth/auth.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) { }
 
-  async validateUser(email: string, password: string) {
+  async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.getUserByEmail(email);
     if (user && user.password === password) {
-      return user;
+      const { password, ...result } = user;
+      return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
   }
 }
