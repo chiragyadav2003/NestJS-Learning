7c95942f95ba9af54060342416e4e93c93c3297c
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-09-25 03:10:05 +0530

    Validation using Class Validator

--- lesson-04/src/users/dto/create-user.dto.ts
+++ lesson-04/src/users/dto/create-user.dto.ts
@@ -1,5 +1,23 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty } from 'class-validator';
+
+enum RoleEnum {
+  ADMIN = 'ADMIN',
+  ENGINEER = 'ENGINEER',
+  INTERN = 'INTERN',
+}
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsEnum(RoleEnum, {
+    message: 'Valid role required!',
+  })
   role: 'ADMIN' | 'ENGINEER' | 'INTERN';
 }
